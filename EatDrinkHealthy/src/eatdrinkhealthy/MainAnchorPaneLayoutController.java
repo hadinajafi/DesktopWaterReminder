63942e2160957d6627c3d3dcf97222fa41d89e1b
hadinajfi77 <hadinajafi.77@gmail.com>
2019-02-11 12:50:57 +0330

    added components to the MainAnchorPaneLayout. TabPane tabPane,AnchorPane root, Label timerCounterLabel, Button skipButton, PieChart statPieChart, AnchorPane tabDailyAnchorPane. Initialize the Pie chart with fixed values. set title for pie chart.

--- EatDrinkHealthy/src/eatdrinkhealthy/MainAnchorPaneLayoutController.java
+++ EatDrinkHealthy/src/eatdrinkhealthy/MainAnchorPaneLayoutController.java
@@ -7,7 +7,16 @@ package eatdrinkhealthy;
 
 import java.net.URL;
 import java.util.ResourceBundle;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.chart.PieChart;
+import javafx.scene.chart.PieChart.Data;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.TabPane;
+import javafx.scene.layout.AnchorPane;
 
 /**
  * FXML Controller class
@@ -15,13 +24,31 @@ import javafx.fxml.Initializable;
  * @author hadin
  */
 public class MainAnchorPaneLayoutController implements Initializable {
+    
+    @FXML
+    private TabPane tabPane;
+    @FXML
+    private AnchorPane root;
+    @FXML
+    private Label timecounterLable;
+    @FXML
+    private Button skipButton;
+    @FXML
+    private PieChart statPieChart;
+    @FXML
+    private AnchorPane tabDailyAnchorPane;
 
     /**
      * Initializes the controller class.
+     * @param url
+     * @param rb
      */
     @Override
     public void initialize(URL url, ResourceBundle rb) {
-        // TODO
+        tabPane.tabMinWidthProperty().bind(root.widthProperty().divide(tabPane.getTabs().size()).subtract(20.5));
+        statPieChart.setTitle("How much you Drunk water today");
+        ObservableList<Data> data = FXCollections.observableArrayList(new PieChart.Data("Water Drunken", 00.75), new PieChart.Data("Didn't Drink yet!", 00.25));
+        statPieChart.setData(data);
     }    
     
 }
