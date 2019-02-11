92dcb3be458a83435856048d0063f71cf54f9796
hadinajfi77 <hadinajafi.77@gmail.com>
2019-02-11 13:23:16 +0330

    add system tray icon to resume run in background

--- EatDrinkHealthy/src/eatdrinkhealthy/EatDrinkHealthy.java
+++ EatDrinkHealthy/src/eatdrinkhealthy/EatDrinkHealthy.java
@@ -5,6 +5,10 @@
  */
 package eatdrinkhealthy;
 
+import java.awt.AWTException;
+import java.awt.Image;
+import java.awt.SystemTray;
+import java.awt.TrayIcon;
 import java.io.IOException;
 import java.util.logging.Level;
 import java.util.logging.Logger;
@@ -13,6 +17,7 @@ import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
 import javafx.scene.layout.AnchorPane;
 import javafx.stage.Stage;
+import javax.imageio.ImageIO;
 
 /**
  *
@@ -22,6 +27,13 @@ public class EatDrinkHealthy extends Application {
 
     @Override
     public void start(Stage primaryStage) {
+        if(SystemTray.isSupported()){
+            System.out.println("tray supported");
+            new EatDrinkHealthy().displayTray();
+        }
+        else{
+            System.err.println("Tray is not supported");
+        }
         AnchorPane mainPane;
         try {
             mainPane = FXMLLoader.load(getClass().getResource("MainAnchorPaneLayout.fxml"));
@@ -29,6 +41,7 @@ public class EatDrinkHealthy extends Application {
             scene.getStylesheets().add(getClass().getResource("mainanchorpanelayout.css").toExternalForm());
             primaryStage.setTitle("Eat & Drink Healthy");
             primaryStage.setResizable(false);
+            primaryStage.getIcons().add(new javafx.scene.image.Image("/icons/water2.png"));
             primaryStage.setScene(scene);
             primaryStage.show();
         } catch (IOException ex) {
@@ -44,5 +57,22 @@ public class EatDrinkHealthy extends Application {
     public static void main(String[] args) {
         launch(args);
     }
-
+    private void displayTray(){
+        SystemTray tray = SystemTray.getSystemTray();
+        Image imageIcon = null;
+        try {
+            imageIcon = ImageIO.read(getClass().getResource("/icons/water1.png"));
+        } catch (IOException ex) {
+            System.err.println(ex.getMessage());
+        }
+        TrayIcon icon = new TrayIcon(imageIcon);
+        icon.setImageAutoSize(true);
+        icon.setToolTip("Tray Icon demo");
+        try {
+            tray.add(icon);
+        } catch (AWTException ex) {
+            System.err.println(ex.getMessage());
+        }
+        icon.displayMessage("Hello World", "Notification demo", TrayIcon.MessageType.INFO);
+    }
 }
