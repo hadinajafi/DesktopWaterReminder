fbbb06a498500cee246baedf3bc9292cd6d6c2d4
hadinajfi77 <hadinajafi.77@gmail.com>
2019-02-16 00:20:35 +0330

    added custom icon to the notification node graphic. added documentation to the code.

--- EatDrinkHealthy/src/eatdrinkhealthy/MainAnchorPaneLayoutController.java
+++ EatDrinkHealthy/src/eatdrinkhealthy/MainAnchorPaneLayoutController.java
@@ -11,11 +11,13 @@ import java.util.Map;
 import java.util.ResourceBundle;
 import java.util.Timer;
 import java.util.TimerTask;
+import javafx.application.Platform;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.Node;
 import javafx.scene.chart.PieChart;
 import javafx.scene.chart.PieChart.Data;
 import javafx.scene.control.Button;
@@ -23,8 +25,12 @@ import javafx.scene.control.Label;
 import javafx.scene.control.Spinner;
 import javafx.scene.control.SpinnerValueFactory;
 import javafx.scene.control.TabPane;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
 
 import javafx.scene.layout.AnchorPane;
+import javafx.util.Duration;
+import org.controlsfx.control.Notifications;
 
 /**
  * FXML Controller class
@@ -75,7 +81,9 @@ public class MainAnchorPaneLayoutController implements Initializable {
         statPieChart.setTitle("How much you Drunk water today");
         ObservableList<Data> data = FXCollections.observableArrayList(new PieChart.Data("Water Drunken", 00.75), new PieChart.Data("Didn't Drink yet!", 00.25));
         statPieChart.setData(data);
-        
+        /*
+        Setting the value factory for the Spinner, increament and decreament works well and the minimum range is 0.
+        */
         timeIntervalTextField.setValueFactory(new SpinnerValueFactory<Integer>() {
             @Override
             public void decrement(int steps) {
@@ -93,16 +101,26 @@ public class MainAnchorPaneLayoutController implements Initializable {
         createTimer();
     }  
     
+    /**
+     * <b>Create Period Timer</b><br>
+     * Start the timer for showing notifications periodically. loadTimer() method used to load the user data.<br>
+     * Two integers from the file will multiply into 60,000 to convert the minute to milliseconds.
+     */
     private void createTimer(){
         timer = new Timer();
         timer.scheduleAtFixedRate(new TimerTask() {
             @Override
             public void run() {
-                new EatDrinkHealthy().displayTray(true);
+                displayNotification(); 
             }
         }, loadTimer()*60000, loadTimer()*60000);
     }
     
+    /**
+     * <b>Load the Timer period from the file</b><br>
+     * FileStreams class object, loads the mapped data and return it. the loaded data is integer.
+     * @return Integer: timer period
+     */
     private int loadTimer(){
         FileStreams streams = new FileStreams();
         userData = streams.getData();
@@ -115,4 +133,17 @@ public class MainAnchorPaneLayoutController implements Initializable {
             return (int)userData.get("timerPeriod");
         }
     }
+    
+    /**
+     * <b>Display Notification</b><br>
+     * pop up notification to show the message to the user
+     * used <i>Platform.runLater()<i> to perform this action on a FX thread. otherwise the timer thread error occurs.
+     */
+    private void displayNotification(){
+        Platform.runLater(() -> {   //creating the FX thread
+            Notifications.create().title("Eat & Drink Healthy").text("Time to drink water Please!").graphic(new ImageView(new Image(getClass().getResourceAsStream("/icons/water2.png")))).hideAfter(Duration.seconds(10)).show();
+        });
+    }
+    
+    
 }
