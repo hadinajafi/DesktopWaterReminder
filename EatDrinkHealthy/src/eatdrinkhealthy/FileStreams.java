070986348e0ffce2ffaaded7844d01186b3570bc
hadinajfi77 <hadinajafi.77@gmail.com>
2019-02-15 18:24:15 +0330

    added com.fasterxml.jackson packages, used Json mapper instead of java.util.properties added minor methods to handle the file place and exception managing.

--- EatDrinkHealthy/src/eatdrinkhealthy/FileStreams.java
+++ EatDrinkHealthy/src/eatdrinkhealthy/FileStreams.java
@@ -5,68 +5,81 @@
  */
 package eatdrinkhealthy;
 
-import java.io.FileReader;
-import java.io.FileWriter;
+import java.io.File;
 import java.io.IOException;
-import java.util.Properties;
+import java.util.Map;
+import com.fasterxml.jackson.core.type.TypeReference;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import java.util.HashMap;
 import java.util.logging.Level;
 import java.util.logging.Logger;
-import javax.swing.JOptionPane;
 
 /**
  *
  * @author hadin
  */
 public class FileStreams {
-    private FileWriter writer;
-    private FileReader reader;
-    private Properties data;
-    private final String filePath=System.getProperty("user.home") + "/.EatDrinkHealthy/data";
+
+    private ObjectMapper mapper;
+    private Map<String, Integer> data;
+    private final String fileName = "eatDrink.data";
+    private final String root = System.getProperty("user.home") + "/.EatDrinkHealthy/";
+    private File dataFile;
 
     public FileStreams() {
-        data = new Properties();
-        
+        data = new HashMap<>();
+        mapper = new ObjectMapper();
+        creatDirectories();
+        dataFile = new File(root + fileName);
     }
-    
-    public void updateTimerPeriod(int value){
-        data.put("timerPeriod", value);
+
+    private void creatDirectories() {
+        File file = new File(root);
+        if (!file.exists()) {
+            file.mkdirs();
+        }
+        dataFile = new File(root + fileName);
+        if (!dataFile.exists()) {
+            try {
+                dataFile.createNewFile();
+            } catch (IOException ex) {
+                System.err.println("Can't write to the disk\n" + ex.getMessage());
+                Logger.getLogger(FileStreams.class.getName()).log(Level.SEVERE, null, ex);
+            }
+        }
+    }
+
+    private void writeData() {
         try {
-            writer = new FileWriter(filePath);
-            reader = new FileReader(filePath);
+            //writer = new FileWriter(dataFile);
+            mapper.writeValue(dataFile, data);
+        } catch (IOException ex) {
+            System.err.println("Can't write on the system disk, \n" + ex.getMessage());
+            Logger.getLogger(FileStreams.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }
+
+    private void readData() {
+        try {
+            data = mapper.readValue(dataFile, new TypeReference<Map<String, Integer>>() {
+            });
         } catch (IOException ex) {
             System.err.println(ex.getMessage());
             Logger.getLogger(FileStreams.class.getName()).log(Level.SEVERE, null, ex);
         }
     }
-    
-    private void writeData(){
-        try {
-            data.store(writer, filePath);
-        } catch (IOException ex) {
-            System.err.println(ex.getMessage());
-            JOptionPane.showMessageDialog(null, "Cannot save data, make sure the application have permission to write on "
-                    + "the user home directory.", "Error While Saving!", JOptionPane.ERROR_MESSAGE);
-            Logger.getLogger(FileStreams.class.getName()).log(Level.SEVERE, null, ex);
-        }
-    }
-    
-    private void readData(){
-        data.clear();
-        try {
-            data.load(reader);
-        } catch (IOException ex) {
-            System.err.println(ex.getMessage());
-            Logger.getLogger(FileStreams.class.getName()).log(Level.SEVERE, null, ex);
-        }
-    }
-    
-    public void setData(Properties data){
+
+    public void setData(Map<String, Integer> data) {
         this.data = data;
         writeData();
     }
-    
-    public Properties getData(){
+
+    public Map<String, Integer> getData() {
         readData();
         return data;
+    }
+
+    public boolean checkExistance() {
+        return dataFile.exists();
     }
 }
