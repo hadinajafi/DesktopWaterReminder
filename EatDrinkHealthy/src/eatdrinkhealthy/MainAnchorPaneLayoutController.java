8461bb0132a3f46e0be7ac1f8a47661c6bc01c07
hadinajfi77 <hadinajafi.77@gmail.com>
2019-02-16 10:19:00 +0330

    added internal timer to count down and display on count down label. added behavior to show count down in hh:mm:ss format. added show message boolean to prevent showing notification on startup and updating timer period.

--- EatDrinkHealthy/src/eatdrinkhealthy/MainAnchorPaneLayoutController.java
+++ EatDrinkHealthy/src/eatdrinkhealthy/MainAnchorPaneLayoutController.java
@@ -31,6 +31,7 @@ import javafx.scene.image.ImageView;
 import javafx.scene.layout.AnchorPane;
 import javafx.util.Duration;
 import org.controlsfx.control.Notifications;
+import sun.security.krb5.internal.rcache.AuthList;
 
 /**
  * FXML Controller class
@@ -38,39 +39,45 @@ import org.controlsfx.control.Notifications;
  * @author hadin
  */
 public class MainAnchorPaneLayoutController implements Initializable {
-    
+
     @FXML
-    private TabPane tabPane;
+    private TabPane tabPane;    //tabbed pane to use a multitab user interface
     @FXML
-    private AnchorPane root;
+    private AnchorPane root;    //root and parent of all components
     @FXML
-    private Label timecounterLable;
+    private Label timecounterLable; //show counting down the current period till next notification. Timer "counter" used for this
     @FXML
-    private Button skipButton;
+    private Button skipButton;  //skip the current period of time and jump to next period
     @FXML
-    private PieChart statPieChart;
+    private PieChart statPieChart;  //showing the stats about how much the user drunk water today
     @FXML
-    private AnchorPane tabDailyAnchorPane;
+    private AnchorPane tabDailyAnchorPane;  //anchor pane inside the daily tab
     @FXML
-    private Spinner<Integer> timeIntervalTextField;
+    private Spinner<Integer> timeIntervalTextField; //Spinner to choose the notifications period
     @FXML
-    private Button timeIntervalSaveBtn;
-    
-    private Map<String, Integer> userData;
-    private Timer timer;
-    
+    private Button timeIntervalSaveBtn; //save the chosen notification period from the spinner and save it to file
+
+    private Map<String, Integer> userData;  //user data such as notification period
+    private Timer timer;    //timer to count down to display notification
+    private Timer counter; //timer for showing counting down on the Timer label
+    private boolean showMessage = false;
+
     @FXML
     void saveApplyBtnAction(ActionEvent event) {
         userData.replace("timerPeriod", Integer.parseInt(timeIntervalTextField.getEditor().getText()));
         FileStreams stream = new FileStreams();
         stream.setData(userData);
+        counter.cancel();
+        counter.purge();
         timer.cancel();
         timer.purge();
+        showMessage = false;
         createTimer();
     }
 
     /**
      * Initializes the controller class.
+     *
      * @param url
      * @param rb
      */
@@ -83,67 +90,124 @@ public class MainAnchorPaneLayoutController implements Initializable {
         statPieChart.setData(data);
         /*
         Setting the value factory for the Spinner, increament and decreament works well and the minimum range is 0.
-        */
+         */
         timeIntervalTextField.setValueFactory(new SpinnerValueFactory<Integer>() {
             @Override
             public void decrement(int steps) {
-                if(!"0".equals(timeIntervalTextField.getEditor().getText()))
-                    timeIntervalTextField.getEditor().setText(String.valueOf(Integer.parseInt(timeIntervalTextField.getEditor().getText())-1));
+                if (!"0".equals(timeIntervalTextField.getEditor().getText())) {
+                    timeIntervalTextField.getEditor().setText(String.valueOf(Integer.parseInt(timeIntervalTextField.getEditor().getText()) - 1));
+                }
             }
 
             @Override
             public void increment(int steps) {
-                timeIntervalTextField.getEditor().setText(String.valueOf(Integer.parseInt(timeIntervalTextField.getEditor().getText())+1));
+                timeIntervalTextField.getEditor().setText(String.valueOf(Integer.parseInt(timeIntervalTextField.getEditor().getText()) + 1));
             }
         });
-        
+
         timeIntervalTextField.getEditor().setText(String.valueOf(loadTimer()));//load default or saved value to the spinner
         createTimer();
-    }  
-    
+    }
+
     /**
      * <b>Create Period Timer</b><br>
-     * Start the timer for showing notifications periodically. loadTimer() method used to load the user data.<br>
-     * Two integers from the file will multiply into 60,000 to convert the minute to milliseconds.
+     * Start the timer for showing notifications periodically. loadTimer()
+     * method used to load the user data.<br>
+     * Two integers from the file will multiply into 60,000 to convert the
+     * minute to milliseconds.
      */
-    private void createTimer(){
+    private void createTimer() {
         timer = new Timer();
         timer.scheduleAtFixedRate(new TimerTask() {
             @Override
             public void run() {
-                displayNotification(); 
+                if (showMessage) {
+                    displayNotification();
+                    counter.cancel();
+                    counter.purge();
+                }
+                //start of internal timer to count down
+                createCounterTimer();
+                showMessage = true;
+                //end of run method
             }
-        }, loadTimer()*60000, loadTimer()*60000);
+        }, 0, loadTimer() * 60000);
     }
-    
+
+    private void createCounterTimer() {
+        counter = new Timer();
+        counter.scheduleAtFixedRate(new TimerTask() {
+            int minutes = loadTimer();
+            int seconds = 0;
+            int hour = minutes / 60;
+
+            @Override
+            public void run() {
+                if (hour > 0 && minutes == 0) {
+                    hour--;
+                    minutes += 59;
+                } else if (minutes > 0 && seconds == 0) {
+                    minutes--;
+                    seconds += 59;
+                } else if (seconds > 0) {
+                    seconds--;
+                } else {
+                    counter.cancel();
+                }
+                Platform.runLater(() -> {
+                    String hh, mm, ss;
+                    if(hour < 10){
+                        hh = "0" + hour;
+                    }else{
+                        hh = String.valueOf(hour);
+                    }
+                    if(minutes < 10){
+                        mm = "0" + minutes;
+                    }else{
+                        mm = String.valueOf(minutes);
+                    }
+                    if(seconds < 10){
+                        ss = "0" + seconds;
+                    }else{
+                        ss = String.valueOf(seconds);
+                    }
+                    timecounterLable.setText(hh + ":" + mm + ":" + ss);
+                });
+            }
+        }, 0, 1000);
+    }
+
     /**
      * <b>Load the Timer period from the file</b><br>
-     * FileStreams class object, loads the mapped data and return it. the loaded data is integer.
+     * FileStreams class object, loads the mapped data and return it. the loaded
+     * data is integer.
+     *
      * @return Integer: timer period
      */
-    private int loadTimer(){
+    private int loadTimer() {
         FileStreams streams = new FileStreams();
         userData = streams.getData();
-        if(!userData.containsKey("timerPeriod") || userData.isEmpty()){
+        if (!userData.containsKey("timerPeriod") || userData.isEmpty()) {
             userData.put("timerPeriod", 60);
             streams.setData(userData);
             return 60;
-        }
-        else{
-            return (int)userData.get("timerPeriod");
+        } else {
+            return (int) userData.get("timerPeriod");
         }
     }
-    
+
     /**
      * <b>Display Notification</b><br>
-     * pop up notification to show the message to the user
-     * used <i>Platform.runLater()<i> to perform this action on a FX thread. otherwise the timer thread error occurs.
+     * pop up notification to show the message to the user.<br>
+     * <i>Platform.runLater()</i> used to perform this action on a FX thread.
+     * otherwise the timer thread error occurs:<br>
+     * <u>The Notification in package controlsfx can't run on a timer thread.
+     * needs FX thread.</u>
      */
-    private void displayNotification(){
+    private void displayNotification() {
         Platform.runLater(() -> {   //creating the FX thread
             Notifications.create().title("Eat & Drink Healthy").text("Time to drink water Please!").graphic(new ImageView(new Image(getClass().getResourceAsStream("/icons/water2.png")))).hideAfter(Duration.seconds(10)).show();
         });
     }
-    
-    
+
 }
